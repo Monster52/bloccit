dcfc4c6a7f6cc31fe7591cea0a95c32a778a8bd0
Ross Waguespack <rosswaguespack@gmail.com>
2016-04-17 11:27:19 -0500

    Assignment 31 Complete

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,16 @@
 class PostsController < ApplicationController
   def index
     @posts = Post.all
+
+    @posts.each do |pst|
+      if pst.id == 1
+        pst.title = "SPAM"
+      elsif pst.id % 5 == 0
+        pst.title = "SPAM"
+      else
+        pst.title
+      end
+    end
   end
 
   def show
