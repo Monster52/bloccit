6c2f2cec3dcc39a78811e4fe04fb47f6b63b0dc6
Xander Miller <xander.miller@gmail.com>
2016-04-20 19:21:24 +0000

    removes question helper specs

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -49,22 +49,6 @@ RSpec.describe QuestionsController, type: :controller do
     end
   end
 
- # describe "POST create" do
- #   it "increases the number of Question by 1" do
- #     expect{ question :create, question: {title: RandomData.random_sentence, body: RandomData.random_paragraph, resolved: true}}.to change(Question,:count).by(1)
- #   end
-#
- #   it "assigns the new question to @question" do
- #     question :create, question: {title: RandomData.random_sentence, body: RandomData.random_paragraph, resolved: true}
- #     expect(assigns(:post)).to eq Question.last
- #   end
-#
- #   it "redirects to the new question" do
- #     question :create, question: {title: RandomData.random_sentence, body: RandomData.random_paragraph, resolved: true}
- #     expect(response).to redirect_to Question.last
- #   end
- # end
-
   describe "GET edit" do
     it "returns https success" do
       get :edit, {id: my_question.id}
