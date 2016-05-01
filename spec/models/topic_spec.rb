926e82d86f8c82885c2021c1296ba7d0e52e6df9
Ross Waguespack <rosswaguespack@gmail.com>
2016-04-30 20:20:05 -0500

    Assignment 35 Complete

--- spec/models/topic_spec.rb
+++ spec/models/topic_spec.rb
@@ -8,6 +8,13 @@ RSpec.describe Topic, type: :model do
 
   it { should have_many(:posts) }
 
+  it { should validate_presence_of(:name) }
+  it { should validate_presence_of(:description) }
+  it { should validate_presence_of(:public) }
+
+  it { should validate_length_of(:name).is_at_least(5) }
+  it { should validate_length_of(:description).is_at_least(15) }
+
   describe "attributes" do
     it { should have_db_column(:name).of_type(:string) }
     it { should have_db_column(:description).of_type(:text) }
