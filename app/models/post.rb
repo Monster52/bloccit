eb88162303756c3d30b597ce6e0808b4d33918a0
Ross Waguespack <rosswaguespack@gmail.com>
2016-05-01 21:11:35 -0500

    Assignment 39 Complete

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,6 +4,8 @@ class Post < ActiveRecord::Base
   belongs_to :user
 
   default_scope { order('created_at DESC') }
+  scope :ordered_by_title, -> { order(title: :asc).unscope(:order) }
+  scope :ordered_by_reverse_created_at, -> { order('ceated_at ASC').unscope(:order) }
 
   validates :title, length: { minimum: 5 }, presence: true
   validates :body, length: { minimum: 20 }, presence: true
