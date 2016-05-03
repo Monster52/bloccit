1f00666ac8f0ca735b60d0942da75eeee78d922b
Xander Miller <xander.miller@gmail.com>
2016-05-03 17:37:26 +0000

    adds null false to topic schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160429162413) do
+ActiveRecord::Schema.define(version: 20160502183950) do
 
   create_table "comments", force: :cascade do |t|
     t.text     "body"
