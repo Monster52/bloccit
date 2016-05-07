e0acb8ce7c5521ac8f402cea147c2d7d20dd801d
Xander Miller <xander.miller@gmail.com>
2016-05-07 01:59:39 +0000

    refactors topic validations

--- spec/models/topic_spec.rb
+++ spec/models/topic_spec.rb
@@ -1,30 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe Topic, type: :model do
-  let(:name) { RandomData.random_sentence }
-  let(:description) { RandomData.random_paragraph }
-  let(:public) { true }
-  let(:topic) { Topic.create!(name: name, description: description) }
-
-
-  it { should validate_presence_of(:name) }
-  it { should validate_presence_of(:description) }
-  it { should validate_presence_of(:public) }
-
-  it { should validate_length_of(:name).is_at_least(5) }
-  it { should validate_length_of(:description).is_at_least(15) }
 
   describe "attributes" do
     it { should have_db_column(:name).of_type(:string) }
     it { should have_db_column(:description).of_type(:text) }
-    it { should have_db_column(:public).of_type(:boolean) }
-
-    it "is public by defalut" do
-      expect(topic.public).to eq(true)
-    end
+    it { should have_db_column(:public).of_type(:boolean).with_options({default: true}) }
   end
   
   describe 'associations' do
     it { should have_many(:posts).dependent(:destroy) }
+  end
+  
+  describe 'validations' do
+    #name
+    it { should validate_presence_of(:name) }
+    it { should validate_length_of(:name).is_at_least(5) }
+    #public
+    it { should validate_presence_of(:public) }
+    #description
+    it { should validate_presence_of(:description) }
+    it { should validate_length_of(:description).is_at_least(15) }
   end
 end
