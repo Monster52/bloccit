0b269e1ca23be164d3708ce08b9720e291121336
Xander Miller <xander.miller@gmail.com>
2016-05-07 19:02:29 +0000

    fixes user spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,36 +3,30 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   let(:user) {User.create!(name: "Bloccit User", email: "user@bloccit.com", password_digest: "password") }
 
-  # Name Test
-  it { should validate_presence_of(:name) }
-  it { should validate_length_of(:name).is_at_least(1) }
-
-  # Email Test
-  it { should validate_presence_of(:email) }
-  it { should validate_uniqueness_of(:email).case_insensitive }
-  it { should validate_length_of(:email).is_at_least(3) }
-  it { should allow_value("user@bloccit.com").for(:email) }
-
-  # Password Test
-  it { should validate_presence_of(:password_digest) }
-  it { should have_secure_password}
-  it { should validate_length_of(:password_digest).is_at_least(6) }
-
-  # Attributes
-  it { should have_db_column(:name).of_type(:string) }
-  it { should have_db_column(:email).of_type(:string) }
-  it { should have_db_column(:password_digest).of_type(:string) }
-
-  describe "invalid user" do
-    let(:user_with_invalid_name) { User.new(name: "", email: "user@bloccit.com", password_digest: "password") }
-    let(:user_with_invalid_email) { User.new(name: "Bloccit User", email: "", password_digest: "password") }
-
-    it "Should be an invalid user due to blank name" do
-      expect(user_with_invalid_name).to_not be_valid
-    end
-
-    it "Should be an invalid user do to blank email" do
-      expect(user_with_invalid_name).to_not be_valid
-    end
+  describe 'attributes' do
+    it { should have_db_column(:name).of_type(:string) }
+    it { should have_db_column(:email).of_type(:string) }
+    it { should have_db_column(:password_digest).of_type(:string) }
   end
+  
+  describe 'associations' do
+  end
+  
+  describe 'validation' do
+    # Name Test
+    it { should validate_presence_of(:name) }
+    it { should validate_length_of(:name).is_at_least(1) }
+  
+    # Email Test
+    it { should validate_presence_of(:email) }
+    it { should validate_uniqueness_of(:email).case_insensitive }
+    it { should validate_length_of(:email).is_at_least(3) }
+    it { should allow_value("user@bloccit.com").for(:email) }
+  
+    # Password Test
+    it { should validate_presence_of(:password_digest) }
+    it { should have_secure_password}
+    it { should validate_length_of(:password_digest).is_at_least(6) }
+  end
+
 end
